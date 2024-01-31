c0440152be6c2b8a28194fd2b6db36ce09c9ed8f
21120353 <90521402+scul0405@users.noreply.github.com>
2024-01-31 16:05:05 +0700

    feat: implement config

--- services/account/cmd/main.go
+++ services/account/cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "log"
+import (
+	"github.com/scul0405/saga-orchestration/services/account/config"
+	"log"
+)
 
 func main() {
 	log.Println("Start account service")
+
+	cfgFile, err := config.LoadConfig("./config/config")
+	if err != nil {
+		log.Fatalf("LoadConfig: %v", err)
+	}
+
+	_, err = config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("ParseConfig: %v", err)
+	}
+
+	log.Println("Load config successfully")
 }
--- services/account/config/config.go
+++ services/account/config/config.go
@@ -1,1 +1,72 @@
 package config
+
+import (
+	"errors"
+	"github.com/spf13/viper"
+	"log"
+	"time"
+)
+
+type Config struct {
+	Service  Service
+	GRPC     GRPC
+	Logger   Logger
+	Postgres Postgres
+}
+
+type Service struct {
+	Name         string
+	Mode         string
+	Debug        bool
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type GRPC struct {
+	Port int
+}
+
+type Logger struct {
+	Development       bool
+	DisableCaller     bool
+	DisableStacktrace bool
+	Encoding          string
+	Level             string
+}
+
+type Postgres struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Dbname   string
+	SSlMode  string
+	PgDriver string
+}
+
+func LoadConfig(filename string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigName(filename)
+	v.AddConfigPath(".")
+	v.AutomaticEnv()
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			return nil, errors.New("config file not found")
+		}
+	}
+
+	return v, nil
+}
+
+func ParseConfig(v *viper.Viper) (*Config, error) {
+	var c Config
+
+	err := v.Unmarshal(&c)
+	if err != nil {
+		log.Printf("unable to decode into struct, %v", err)
+		return nil, err
+	}
+
+	return &c, nil
+}
