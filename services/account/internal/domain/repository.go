8fbb625656b8dc411efa8c0afccb09ad09d43732
scul0405 <90521402+scul0405@users.noreply.github.com>
2024-02-01 17:55:56 +0700

    chore: update repository

--- services/account/internal/domain/repository.go
+++ services/account/internal/domain/repository.go
@@ -7,21 +7,8 @@ import (
 
 // CustomerRepository is the customer repository interface
 type CustomerRepository interface {
-	GetCustomerPersonalInfo(ctx context.Context, customerID uint64) (*CustomerPersonalInfo, error)
-	GetCustomerDeliveryInfo(ctx context.Context, customerID uint64) (*CustomerDeliveryInfo, error)
+	GetCustomerPersonalInfo(ctx context.Context, customerID uint64) (*valueobject.CustomerPersonalInfo, error)
+	GetCustomerDeliveryInfo(ctx context.Context, customerID uint64) (*valueobject.CustomerDeliveryInfo, error)
 	UpdateCustomerPersonalInfo(ctx context.Context, customerID uint64, personalInfo *valueobject.CustomerPersonalInfo) error
-	UpdateCustomerShippingInfo(ctx context.Context, customerID uint64, shippingInfo *valueobject.CustomerDeliveryInfo) error
-}
-
-// CustomerPersonalInfo os customer personal info type
-type CustomerPersonalInfo struct {
-	FirstName string
-	LastName  string
-	Email     string
-}
-
-// CustomerDeliveryInfo os customer shipping info type
-type CustomerDeliveryInfo struct {
-	Address     string
-	PhoneNumber string
+	UpdateCustomerDeliveryInfo(ctx context.Context, customerID uint64, deliveryInfo *valueobject.CustomerDeliveryInfo) error
 }
