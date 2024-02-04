3121bff774f995f811e772f927aaedf2892faa3b
scul0405 <90521402+scul0405@users.noreply.github.com>
2024-02-04 16:08:50 +0700

    fix: update app run to main

--- services/account/cmd/api/main.go
+++ services/account/cmd/api/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"context"
 	"github.com/scul0405/saga-orchestration/services/account/config"
-	"github.com/scul0405/saga-orchestration/services/account/internal/app"
+	"github.com/scul0405/saga-orchestration/services/account/internal/infrastructure/db/postgres"
 	"github.com/scul0405/saga-orchestration/services/account/internal/infrastructure/logger"
+	portgrpc "github.com/scul0405/saga-orchestration/services/account/internal/ports/grpc"
+	porthttp "github.com/scul0405/saga-orchestration/services/account/internal/ports/http"
+	"github.com/scul0405/saga-orchestration/services/account/internal/repository/postgres_repo"
+	customersvc "github.com/scul0405/saga-orchestration/services/account/internal/service/account"
+	authsvc "github.com/scul0405/saga-orchestration/services/account/internal/service/auth"
+	"github.com/scul0405/saga-orchestration/services/account/pkg"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	shutdownTimeout = 5 * time.Second
 )
 
 func main() {
@@ -20,9 +35,86 @@ func main() {
 		log.Fatalf("ParseConfig: %v", err)
 	}
 
-	appLogger := logger.NewApiLogger(cfg)
-	appLogger.InitLogger()
-	appLogger.Infof("Service Name: %s, LogLevel: %s, Mode: %s", cfg.Service.Name, cfg.Logger.Level, cfg.Service.Mode)
+	apiLogger := logger.NewApiLogger(cfg)
+	apiLogger.InitLogger()
+	apiLogger.Infof("Service Name: %s, LogLevel: %s, Mode: %s", cfg.Service.Name, cfg.Logger.Level, cfg.Service.Mode)
 
-	appLogger.Fatal(app.NewApp(cfg, appLogger).Run())
+	doneCh := make(chan struct{}) // for graceful shutdown
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	defer cancel()
+
+	// connect postgres
+	psqlDB, err := postgres.NewPsqlDB(cfg)
+	if err != nil {
+		apiLogger.Fatal(err)
+	}
+	defer func() {
+		db, err := psqlDB.DB()
+		if err = db.Close(); err != nil {
+			apiLogger.Errorf("Close db err: %v", err)
+		}
+	}()
+
+	// run migration
+	apiLogger.Infof("Run migrations with config: %+v", cfg.Migration)
+	err = postgres.NewMigrator(psqlDB).Migrate(cfg.Migration)
+	if err != nil {
+		apiLogger.Errorf("RunMigrations err: %v", err)
+		apiLogger.Fatal(err)
+	}
+	apiLogger.Info("Migrations successfully")
+
+	// create repositories
+	customerRepo := postgres_repo.NewCustomerRepositoryImpl(psqlDB)
+	jwtAuthRepo := postgres_repo.NewJwtAuthRepositoryImpl(psqlDB)
+
+	// create sony flake
+	sf, err := pkg.NewSonyFlake()
+	if err != nil {
+		apiLogger.Fatal(err)
+	}
+
+	// create services
+	customerService := customersvc.NewCustomerService(customerRepo, apiLogger)
+	authService := authsvc.NewJWTAuthService(cfg.JWTConfig, jwtAuthRepo, apiLogger, sf)
+
+	// create http server
+	engine := porthttp.NewEngine(cfg.HTTP)
+	router := porthttp.NewRouter(authService, customerService)
+	httpServer := porthttp.NewHTTPServer(cfg.HTTP, apiLogger, engine, router)
+
+	// run http server
+	go func() {
+		if err := httpServer.Run(); err != nil {
+			apiLogger.Fatalf("Run http server err: %v", err)
+		}
+	}()
+
+	// create grpc server
+	grpcServer := portgrpc.NewGRPCServer(cfg.GRPC, authService)
+
+	// run grpc server
+	go func() {
+		if err := grpcServer.Run(); err != nil {
+			apiLogger.Fatalf("Run grpc server err: %v", err)
+		}
+	}()
+
+	// graceful shutdown
+	<-ctx.Done()
+	go func() {
+		time.Sleep(shutdownTimeout)
+		apiLogger.Infof("Shutdown timeout exceeded, force shutdown")
+
+		err = httpServer.GracefulStop(ctx)
+		if err != nil {
+			apiLogger.Errorf("httpServer.GracefulStop err: %v", err)
+		}
+
+		grpcServer.GracefulStop()
+		doneCh <- struct{}{}
+	}()
+
+	<-doneCh
+	apiLogger.Infof("%s app exited properly", cfg.Service.Name)
 }
