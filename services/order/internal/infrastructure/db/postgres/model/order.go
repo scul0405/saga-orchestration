a3ccc18015c0f53a9639c54f0e8435d49853d52e
scul0405 <90521402+scul0405@users.noreply.github.com>
2024-02-18 16:48:13 +0700

    chore: update order model

--- services/order/internal/infrastructure/db/postgres/model/order.go
+++ services/order/internal/infrastructure/db/postgres/model/order.go
@@ -3,7 +3,7 @@ package model
 type Order struct {
 	ID         uint64 `gorm:"primaryKey"`
 	ProductID  uint64 `gorm:"primaryKey"`
-	Amount     uint64 `gorm:"not null"`
+	Quantity   uint64 `gorm:"not null"`
 	CustomerID uint64 `gorm:"not null"`
 	UpdatedAt  int64  `gorm:"autoUpdateTime:milli"`
 	CreatedAt  int64  `gorm:"autoCreateTime:milli"`
