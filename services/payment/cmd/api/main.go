64b71613597e4a0b5ccd581ee816f9e21cad5628
scul0405 <90521402+scul0405@users.noreply.github.com>
2024-02-19 14:18:15 +0700

    feat: implement db conn and logger

---
+++ services/payment/cmd/api/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/scul0405/saga-orchestration/services/payment/config"
+	"github.com/scul0405/saga-orchestration/services/payment/internal/infrastructure/db/postgres"
+	"github.com/scul0405/saga-orchestration/services/payment/internal/infrastructure/logger"
+	"log"
+	"time"
+)
+
+const (
+	shutdownTimeout = 5 * time.Second
+)
+
+func main() {
+	log.Println("Start order service...")
+
+	cfgFile, err := config.LoadConfig("./config/config")
+	if err != nil {
+		log.Fatalf("LoadConfig: %v", err)
+	}
+
+	cfg, err := config.ParseConfig(cfgFile)
+	if err != nil {
+		log.Fatalf("ParseConfig: %v", err)
+	}
+
+	apiLogger := logger.NewApiLogger(cfg)
+	apiLogger.InitLogger()
+	apiLogger.Infof("Service Name: %s, LogLevel: %s, Mode: %s", cfg.Service.Name, cfg.Logger.Level, cfg.Service.Mode)
+
+	// connect postgres
+	psqlDB, err := postgres.NewPsqlDB(cfg)
+	if err != nil {
+		apiLogger.Fatal(err)
+	}
+	defer func() {
+		db, err := psqlDB.DB()
+		if err = db.Close(); err != nil {
+			apiLogger.Errorf("Close db err: %v", err)
+		}
+	}()
+
+	// run migration
+	apiLogger.Infof("Run migrations with config: %+v", cfg.Migration)
+	err = postgres.NewMigrator(psqlDB).Migrate(cfg.Migration)
+	if err != nil {
+		apiLogger.Errorf("RunMigrations err: %v", err)
+		apiLogger.Fatal(err)
+	}
+	apiLogger.Info("Migrations successfully")
+}
